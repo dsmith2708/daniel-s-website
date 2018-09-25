5e107ccc0376deb0a0559631f47012eb995ba1a1
Daniel Smith <danielsmith.2796@googlemail.com>
2018-09-25 16:41:57 +0100

    experimenting with about me

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
 import { HobbiesAndInterestsComponent } from './components/hobbies-and-interests/hobbies-and-interests.component';
 import { MyCvComponent } from './components/my-cv/my-cv.component';
 import { PreviousProjectsComponent } from './components/previous-projects/previous-projects.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: AboutMeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'about-me', component: AboutMeComponent },
     { path: 'hobbies-interests', component: HobbiesAndInterestsComponent },
     { path: 'my-cv', component: MyCvComponent },
