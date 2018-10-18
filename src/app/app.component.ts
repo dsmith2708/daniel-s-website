270dded7c23f392bb8399ea71cf91ea8b8910e20
Daniel Smith <danielsmith.2796@googlemail.com>
2018-10-18 14:01:46 +0100

    page title updates when route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,31 @@ import { MatSidenavModule } from '@angular/material';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pageTitles = [
+    {route: '/home', name: 'Home'},
+    {route: '/about-me', name: 'About me'},
+    {route: '/hobbies-interests', name: 'Hobbies and Interests'},
+    {route: '/my-cv', name: 'My CV'},
+    {route: '/previous-projects', name: 'Previous Projects'}
+  ] ;
   title = 'Daniel Smith Portfolio';
-
   pageTitle = 'Home';
+
+  constructor(activatedRoute: ActivatedRoute, public router: Router) {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      debounceTime(1000)
+    ).subscribe(
+      x => {
+        this.setPageTitle((x as any).urlAfterRedirects);
+      }
+    );
+  }
+
+  setPageTitle(currentRoute: string) {
+     this.pageTitle = this.pageTitles.filter((page) => {
+      return page.route === currentRoute;
+    })[0].name;
+  }
+
 }
